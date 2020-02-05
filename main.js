cc3111106912874f08a874d17049fd0e683d3064
Aman Agrawal <amanagrawal2098@gmail.com>
2020-02-05 22:05:41 +0530

    file with http module

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
-function student(){
-    this.name="aman";
-    this.age=22;
-}
-//making a prototype to add extra data as inheritance
-student.prototype={
-    address:"adress",
-    getname:function(){
-        return this.name;
-    }
-}
-var stud=new student();
-console.log(stud.address);
-console.log(stud.getname());
+const http=require("http");
+const server=http.createServer(function(req,res)
+{
+    res.writeHead(200,{"content-type":"text/plain"});//even though we are writing html it will convert it to plain text
+    res.write("<h1>Node js tutorial running</h1>");
+    res.end();
+}).listen(3000);
