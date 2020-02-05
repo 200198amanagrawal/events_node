7c1e06dd7021d30960e23d6254f3156fb4d0f5d0
Aman Agrawal <amanagrawal2098@gmail.com>
2020-02-05 23:17:05 +0530

    the event

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-const http=require("http");
-const fs=require("fs");//creating the file and saving the data.
-const server=http.createServer(function(req,res)
-{
-    fs.readFile(__dirname+"/hello.txt","utf-8",(err,data)=>{//remember one thing whenever we are writing the code of http req we have to put inside the fs..
-        res.writeHead(200,{"content-type":"text/plain"});
-        res.write(data);
-        res.end();
-        });
-}).listen(3000);
-
+//events are used to control the html browser page(js page).
+const events=require("events");
+const event=new events.EventEmitter;
+event.on("click",()=>console.log("Hello world"));
+event.emit("click");
+event.on("clickadd",(a,b)=>console.log(b+a));
+event.emit("clickadd",4,5);
